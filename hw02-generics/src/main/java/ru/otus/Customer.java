07213f97b22dafa20381e44bc914eae3bbbbc512
kenbaev <k.enbaev@mail.ru>
2021-10-19 23:49:42 +0500

    Fix code format

--- hw02-generics/src/main/java/ru/otus/Customer.java
+++ hw02-generics/src/main/java/ru/otus/Customer.java
@@ -1,6 +1,6 @@
 package ru.otus;
 
-public class Customer implements Comparable<Customer>{
+public class Customer implements Comparable<Customer> {
     private final long id;
     private String name;
     private long scores;
